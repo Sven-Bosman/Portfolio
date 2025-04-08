e2e49c08386a894c9c8e02cac15e7e7968daa686
Sven Bosman <sbos279@gmail.com>
2025-04-08 11:30:18 +0200

    fixed responsive nav
    verandert van type nav wanneer groter of kleiner

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let menu = document.getElementById("menu");
+let btn = document.getElementById("btn");
+let drop = document.getElementById("drop");
+let list = document.getElementById("list");
+
+btn.addEventListener("click",  dropdown); 
+
+setInterval(check, 1000);
+
+
+
+function dropdown(){
+   if(drop.style.display == "none"){
+
+       drop.style.display = "flex";
+   }
+   else{
+       drop.style.display = "none";
+   }
+}
+
+
+
+function check(){
+    var windowWidth = window.innerWidth;
+
+    if (windowWidth < 1280) {
+        menu.style.display = "none";
+    }
+    else {
+        menu.style.display = "flex";
+    }
+}
+
+
