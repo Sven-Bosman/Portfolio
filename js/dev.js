65925eb8144ed29756cb1b93f710e9739205161d
Sven Bosman <sbos279@gmail.com>
2025-05-26 12:05:15 +0200

    improved response

---
+++ js/dev.js
@@ -0,0 +1,12 @@
+let H_screen = screen.height
+let W_screen = screen.width
+
+
+// setInterval(getScreenSize, 1000);
+
+function getScreenSize() {
+    console.log('height: '+ H_screen)
+    console.log('width: '+ W_screen)
+}
+
+
